d2d5302d68597740d3810021d2de9dc06be3ea40
Palakpreet-Kaur <palakpreet1553.becse24@chitkara.edu.in>
2025-04-25 20:30:52 +0530

    CSS files added

---
+++ javascript/sidebar.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggle-btn');
+    const sidebar = document.getElementById('sidebar');
+  
+    toggleBtn.addEventListener('click', () => {
+      sidebar.classList.toggle('collapsed');
+    });
+  });
+  
